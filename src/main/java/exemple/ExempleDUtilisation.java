eb8880482bcb5f080e278b5f3da364253c62560c
Wassim_Aityoucef <58562600+Wasslamenace@users.noreply.github.com>
2020-12-08 17:33:29 +0100

    Commit : terminé 2.0

--- src/main/java/exemple/ExempleDUtilisation.java
+++ src/main/java/exemple/ExempleDUtilisation.java
@@ -4,6 +4,12 @@ import bandeau.Bandeau;
 import java.awt.Color;
 import java.awt.Font;
 
+
+// Ce TP a été très difficile à mettre en oeuvre. Dans un premier temps je ne sais pas pourquoi mais Netbeans affinche "La syntaxe est inccorecte" je n'arrive pas à faire fonctionner le programme meme s'il n'y a aucune erreur.
+// Second point, je n'ai pas réussi à ouvrir le mdzip, MagicDraw me demande une licence, malgré le fait que j'ai dl la version d'essai, j'ai desinstaller réinstaller sans aucun succès. 
+
+
+
 public class ExempleDUtilisation {
     
 public static void main(String[] args) {
